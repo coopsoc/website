9444e3f72d6fc07de2ea9b8212ae8d6135b9d009
Hanyuan Li <hanyuan.work@gmail.com>
2022-06-02 13:39:04 +1000

    feat(deploy): added .github/deploy.yml

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? "/coopsoc.com.au/" : "",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
