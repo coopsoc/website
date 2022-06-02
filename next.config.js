951c2a0773217f6a6c2ffb9975f057d8ca40dd10
Hanyuan Li <hanyuan.work@gmail.com>
2022-06-02 14:10:24 +1000

    fix(config): patch unoptimised images issue

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: isProd ? "/coopsoc.com.au/" : "",
+  images: {
+    loader: "akamai",
+    path: "/"
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
