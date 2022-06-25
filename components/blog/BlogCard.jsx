480d4da04669b46837ca0f9f99a78f5295c54e05
Hanyuan Li <hanyuan.work@gmail.com>
2022-06-25 22:08:34 +1000

    feat(blog): created BlogCard component, refactoring

---
+++ components/blog/BlogCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+import { Button, Col, Container } from "reactstrap";
+
+const BlogCard = ({ title, description, img, href }) => {
+  return (
+    <Col lg="5">
+      <Container className="py-lg-md d-flex">
+        <div className="card">
+          <Image alt={title} src={img} className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title" style={{ height: '50px' }}><b>{title}</b></h5>
+            <p className="card-text" style={{ height: '100px' }}>{description}</p>
+
+            <Button
+              className="mt-4"
+              color="index"
+              href={href}
+              target="_Blank"
+            >
+              Learn more
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </Col>
+  )
+};
+
+export default BlogCard;
--- pages/blog.jsx
+++ pages/blog.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 
+import BlogCard from "components/blog/BlogCard.jsx";
+
 // Images
 import Stonks from "public/img/pubs/stonks.jpeg";
-
 import Connected from "public/img/pubs/connected.jpg";
 import Placement from "public/img/pubs/placement.jpg";
 
@@ -22,7 +23,6 @@ import "animate.css";
 const Blog = () => {
   // FIXME: style up blog posts so they mesh with rest of website
   // TODO: consider converting blogs to Markdown or MDX
-  // TODO: move cards to their own component
   return (
     <>
       <Head>
@@ -30,76 +30,34 @@ const Blog = () => {
       </Head>
 
       <section className="section section-lg">
-        <Row className="justify-content-center text-center ">
+        <Row className="justify-content-center text-center">
           <Col lg="8">
             <h1 className="animate__animated animate__fadeInDown animate__fast">BLOG POSTS</h1>
           </Col>
         </Row>
       </section>
 
-      <section className="section section-lg mt--200">
-        <Row className="justify-content-center text-center ">
-          <Col lg="5">
-            <br></br>
-            <br></br>
-            <Container className="py-lg-md d-flex">
-              <div className="card">
-                <Image alt="Why Most Young People Should Start Investing" src={Stonks} className="card-img-top" />
-                <div className="card-body" >
-                  <h5 className="card-title" style={{ height: '50px' }}><b>Why Most Young People Should Start Investing</b></h5>
-                  <p className="card-text" style={{ height: '100px' }}>Ever considered learning to invest? This article by Ethan Wong (BIS21) - a passionate young investor himself - outlines why you should, and how you can make a start on it today!</p>
+      <section className="section section-lg">
+        <Row className="justify-content-center text-center mb-lg">
+          <BlogCard
+            title="Why Most Young People Should Start Investing"
+            description="Ever considered learning to invest? This article by Ethan Wong (BIS21) - a passionate young investor himself - outlines why you should, and how you can make a start on it today!"
+            img={Stonks}
+            href="https://ethwong.medium.com/why-most-young-people-should-start-investing-6ea64448ef6b" />
 
-                  <Button
-                    className="mt-4"
-                    color="index"
-                    href="https://ethwong.medium.com/why-most-young-people-should-start-investing-6ea64448ef6b"
-                    target="_Blank"
-                  >
-                    Learn more
-                  </Button>
-                </div>
-              </div>
-            </Container>
-          </Col>
-
-          <Col lg="5">
-            <br></br>
-            <br></br>
-            <Container className="py-lg-md d-flex">
-              <div className="card">
-                <Image alt="Social Distancing Without the Socially Distant" src={Connected} className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title" style={{ height: '50px' }}><b>Social Distancing Without the Socially Distant</b></h5>
-                  <p className="card-text" style={{ height: '100px' }}>How can we stay connected during isolation?</p>
-
-                  <Button className="mt-4" color="index" href="/blog/social-distance">
-                    Learn more
-                  </Button>
-                </div>
-
-              </div>
-            </Container>
-          </Col>
+          <BlogCard
+            title="Social Distancing Without the Socially Distant"
+            description="How can we stay connected during isolation?"
+            img={Connected}
+            href="/blog/social-distance" />
         </Row>
 
-        <Row className="justify-content-center text-center">
-          <Col lg="5">
-            <br></br>
-            <br></br>
-            <Container className="py-lg-md d-flex">
-              <div className="card">
-                <Image alt="First Placement Experiences" src={Placement} className="card-img-top" />
-                <div className="card-body" >
-                  <h5 className="card-title" style={{ height: '50px' }}><b>First Placement Experiences</b></h5>
-                  <p className="card-text" style={{ height: '100px' }}>Read about some of our past Exec&apos;s placement experiences!</p>
-
-                  <Button className="mt-4" color="index" href="/blog/placement">
-                    Learn more
-                  </Button>
-                </div>
-              </div>
-            </Container>
-          </Col>
+        <Row className="justify-content-center text-center mb-lg">
+          <BlogCard
+            title="First Placement Experiences"
+            description="Read about some of our past Exec's placement experiences!"
+            img={Placement}
+            href="/blog/placement" />
         </Row>
       </section>
     </>
