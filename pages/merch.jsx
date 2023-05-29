6dc2bfcb92924bacac1bc883b54bac049a9aff54
Will <theawesomew@gmail.com>
2023-05-29 10:41:41 +1000

    Added a skeleton merch page

---
+++ pages/merch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+const Merch = () => {
+	return (
+		<>
+      			<Head>
+        			<title>Charity | UNSW Co-op Society</title>
+     			</Head>
+		</>
+	);
+};
+
+export default Merch;
