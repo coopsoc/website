e3f459042294fb1dda18831037564f5fc61fff98
Will Kennedy <will.kennedy@wisetechglobal.com>
2023-06-07 18:28:51 +1000

    Hopefully... this works

--- pages/checkout/[slug].jsx
+++ pages/checkout/[slug].jsx
@@ -5,9 +5,10 @@ import { useRouter } from 'next/router';
 import { Elements, PaymentElement } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
+const stripePromise = loadStripe("pk_live_51N7xWEKWz42bhxUE0OeOgsSQoeFdMRPXvxelNmH2U9TkHjsCC1wLE1O6nYvArlihn7regSqjiHVTk89atbSNL2hc00c4XYqVw3");
+
 const Payment = () => {
   const router = useRouter();
-  const stripePromise = loadStripe("pk_live_51N7xWEKWz42bhxUE0OeOgsSQoeFdMRPXvxelNmH2U9TkHjsCC1wLE1O6nYvArlihn7regSqjiHVTk89atbSNL2hc00c4XYqVw3");
 
   const options = {
     clientSecret: `{{${router.query.slug}}}`
