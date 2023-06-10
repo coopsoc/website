2b0d626bb7648a9992f4d48764464864e3f4b45a
scorpiontornado <langfordn@icloud.com>
2023-06-10 14:13:58 +1000

    Cart length pill v1

--- components/merch/CartSymbol.jsx
+++ components/merch/CartSymbol.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import styles from "styles/modules/Merch.module.scss";
+
 const CartSymbol = ({ click }) => {
   return (
     <>
       <i className="fa badge fa-2x" onClick={click} >&#xf07a;</i>
+      <span className={styles[`badge`]}> 5 </span>
     </>
   );
 };
