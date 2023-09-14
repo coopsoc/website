fd4321dff32f1485fddb9be1f52a60d674e395b7
Will Kennedy <will.kennedy@wisetechglobal.com>
2023-09-14 17:34:01 +1000

    Added passwords page which is incomplete, removed merch

---
+++ pages/passwords.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+const Passwords = () => {
+  return (
+    <>
+      <Head>
+        <title>Passwords | UNSW Co-op Society</title>
+      </Head>
+      <button className={'submit'}>BRUH!</button>
+      <Script src="/scripts/test.js"></Script>
+    </>
+  )
+};
+
+export default Passwords;
