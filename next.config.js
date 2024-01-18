22e43b12f4028cc024bfc314841732047bcccc5b
Nicholas Langford <langfordn@icloud.com>
2024-01-19 00:20:21 +1100

    Remove next redirects

--- next.config.js
+++ next.config.js
@@ -11,20 +11,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  redirects: async () => [
-    {
-      source: "/calendar",
-      destination:
-        "webcal://calendar.google.com/calendar/ical/pnpcv7pttn3n36herr12kb0lm8%40group.calendar.google.com/public/basic.ics",
-      permanent: false,
-    },
-    {
-      // Short link for first year facebook group
-      source: "/fb",
-      destination: "https://www.facebook.com/groups/883034036564419/",
-      permanent: false,
-    },
-  ],
 };
 
 module.exports = nextConfig;
