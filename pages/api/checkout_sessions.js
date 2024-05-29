2d3292fabfe02e5b8036d6c4de2e94625cccc4aa
Nicholas Langford <n.langford@student.unsw.edu.au>
2024-05-29 23:40:16 +1000

    Initial, WIP implementation of checkout.jsx

---
+++ pages/api/checkout_sessions.js
@@ -0,0 +1,51 @@
+// From https://docs.stripe.com/checkout/embedded/quickstart?client=next
+
+// TODO add secret key to env
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      try {
+        // Create Checkout Sessions from body params.
+        const session = await stripe.checkout.sessions.create({
+          ui_mode: "embedded",
+          line_items: [
+            {
+              // Provide the exact Price ID (for example, pr_1234) of
+              // the product you want to sell
+              price: "{{PRICE_ID}}",
+              quantity: 1,
+              // TODO - get info from cart / price IDs
+              //  (could get the price for the given product with stripe API)
+            },
+          ],
+          mode: "payment",
+          // TODO update return endpoint as necessary (keep {CHECKOUT_SESSION_ID})
+          return_url: `${req.headers.origin}/return?session_id={CHECKOUT_SESSION_ID}`,
+        });
+
+        res.send({ clientSecret: session.client_secret });
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+      break;
+    case "GET":
+      try {
+        const session = await stripe.checkout.sessions.retrieve(
+          req.query.session_id,
+        );
+
+        res.send({
+          status: session.status,
+          customer_email: session.customer_details.email,
+        });
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+      break;
+    default:
+      res.setHeader("Allow", req.method);
+      res.status(405).end("Method Not Allowed");
+  }
+}
---
+++ pages/checkout.jsx
@@ -0,0 +1,34 @@
+// Modified from:
+//    https://docs.stripe.com/checkout/embedded/quickstart?client=next#initialize-checkout
+
+import React, { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+);
+
+export default function App() {
+  const fetchClientSecret = useCallback(() => {
+    // Create a Checkout Session
+    return fetch("/api/checkout_sessions", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => data.clientSecret);
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
