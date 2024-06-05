d8308c0173998d1d12fc7f156147b33ccbd63f53
Nicholas Langford <n.langford@student.unsw.edu.au>
2024-06-06 03:47:07 +1000

    Clean up checkout index, identify CSP issue

--- pages/checkout/index.jsx
+++ pages/checkout/index.jsx
@@ -5,7 +5,7 @@
 // - https://github.com/stripe/stripe-js
 // - https://vercel.com/guides/getting-started-with-nextjs-typescript-stripe
 
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
@@ -28,42 +28,12 @@ export default function App() {
 
   const options = { fetchClientSecret };
 
+  // TODO - fix CSP and uncomment: https://stripe.com/docs/security/guide#content-security-policy
   return (
     <div id="checkout">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      {/* <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
         <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
+      </EmbeddedCheckoutProvider> */}
     </div>
   );
 }
-
-// import React, { useCallback } from "react";
-// import {
-//   EmbeddedCheckoutProvider,
-//   EmbeddedCheckout,
-// } from "@stripe/react-stripe-js";
-// import getStripe from "scripts/get-stripejs";
-
-// const Checkout = () => {
-//   const fetchClientSecret = useCallback(() => {
-//     // Create a Checkout Session
-//     return fetch("/api/checkout_sessions", {
-//       method: "POST",
-//     })
-//       .then((res) => res.json())
-//       .then((data) => data.clientSecret);
-//   }, []);
-
-//   const options = { fetchClientSecret };
-//   const stripePromise = getStripe();
-
-//   return (
-//     <div id="checkout">
-//       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-//         <EmbeddedCheckout />
-//       </EmbeddedCheckoutProvider>
-//     </div>
-//   );
-// };
-
-// export default Checkout;
