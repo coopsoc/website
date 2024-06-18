110d2a4bccdb36d55f80314fdabe7114830e8f3f
Lucas <me@lhvy.dev>
2024-06-18 12:04:36 +1000

    Implement checkout with chosen cart items

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -1,35 +1,58 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
 
 // From https://docs.stripe.com/checkout/embedded/quickstart?client=next
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const itemSchema = z.object({
+  price: z.string(),
+  quantity: z.number().int(),
+});
+
+const bodySchema = z.object({
+  items: z.array(itemSchema),
+});
+
+type Body = z.infer<typeof bodySchema>;
+
+type ResponseData = {
+  error?: string;
+  clientSecret?: string;
+  status?: string;
+  customer_email?: string;
+};
 
 // TODO update secret key in .env.local
 // TODO (opt) - change from CJS/require to MJS/loadStripe - see checkout/index.tsx
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
   switch (req.method) {
     case "POST":
       try {
+        const result = bodySchema.safeParse(JSON.parse(req.body));
+        if (!result.success) {
+          return res
+            .status(400)
+            .json({ error: "Invalid request body. Please try again." });
+        }
+        const validBody: Body = result.data;
+
         // Create Checkout Sessions from body params.
         const session = await stripe.checkout.sessions.create({
           ui_mode: "embedded",
-          line_items: [
-            {
-              // Provide the exact Price ID (for example, pr_1234) of
-              // the product you want to sell
-              price: "price_1PQoBSKWz42bhxUEodZ18Z6I",
-              quantity: 1,
-              // TODO - get info from cart / price IDs
-              //  (could get the price for the given product with stripe API)
-            },
-          ],
+          line_items: validBody.items,
           mode: "payment",
           return_url: `${req.headers.origin}/checkout/return?session_id={CHECKOUT_SESSION_ID}`,
         });
 
         res.send({ clientSecret: session.client_secret });
-      } catch (err) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
         res.status(err.statusCode || 500).json(err.message);
       }
       break;
@@ -43,12 +66,14 @@ export default async function handler(req, res) {
           status: session.status,
           customer_email: session.customer_details.email,
         });
-      } catch (err) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
         res.status(err.statusCode || 500).json(err.message);
       }
       break;
     default:
-      res.setHeader("Allow", req.method);
+      // TODO: Do we need this?
+      // res.setHeader("Allow", req.method);
       res.status(405).end("Method Not Allowed");
   }
 }
--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -10,7 +10,8 @@ import {
   EmbeddedCheckoutProvider,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { useCallback } from "react";
+import { CartItemWithDetail } from "api/merch";
+import { useCallback, useEffect, useState } from "react";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "NO_KEY_FOUND",
@@ -18,24 +19,48 @@ const stripePromise = loadStripe(
 
 // TODO: Do not display the checkout unless client secret is fetched, and cart is not empty
 const Checkout = () => {
+  const [props, setProps] = useState<CartItemWithDetail[]>([]);
+
+  useEffect(() => {
+    const cart = localStorage.getItem("cart");
+    let items: CartItemWithDetail[] = [];
+    if (cart !== null) {
+      items = JSON.parse(cart);
+    } else {
+      items = [];
+    }
+    setProps(items);
+  }, []);
+
   const fetchClientSecret = useCallback(async () => {
     // Create a Checkout Session
     const res = await fetch("/api/checkout_sessions", {
       method: "POST",
+      body: JSON.stringify({
+        items: props.map((item) => ({
+          price: item.price.id,
+          quantity: item.qty,
+        })),
+      }),
     });
     const data = await res.json();
     return data.clientSecret;
-  }, []);
+  }, [props]);
 
-  const options = { fetchClientSecret };
+  if (props.length) {
+    const options = { fetchClientSecret };
 
-  return (
-    <div id="checkout" className="mb-4 mb-xl-5">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-        <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
-    </div>
-  );
+    return (
+      <div id="checkout" className="mb-4 mb-xl-5">
+        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+          <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+      </div>
+    );
+  }
+
+  // TODO: Improve this case
+  return <div>Cart is empty</div>;
 };
 
 export default Checkout;
